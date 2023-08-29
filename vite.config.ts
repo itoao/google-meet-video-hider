d93ba5f66f4d701b34873399a3607483e613f37c
aoito <ao.ito@tabian.co>
2023-08-30 00:29:52 +0900

    🔧 manifest.tsをvite.configにかく

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,37 @@
-import { crx } from '@crxjs/vite-plugin';
+import { crx, defineManifest } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
-import { join, resolve } from 'path';
 import { defineConfig } from 'vite';
-import manifest from './src/manifest';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'Google Meet Video Hider',
+  version: '1.0',
+  description: 'Hide video of participants during screen sharing in Google Meet',
+  permissions: ['https://meet.google.com/*', 'storage'],
+  action: {
+    default_popup: 'popup.html',
+    // default_icon: {
+    //   '16': 'icons/icon16.png',
+    //   '48': 'icons/icon48.png',
+    //   '128': 'icons/icon128.png',
+    // },
+  },
+  background: {
+    service_worker: 'src/background/index.ts',
+  },
+  content_scripts: [
+    {
+      matches: ['https://meet.google.com/*'],
+      js: ['src/content/index.tsx'],
+    },
+  ],
+  // icons: {
+  //   '16': 'icons/icon16.png',
+  //   '48': 'icons/icon48.png',
+  //   '128': 'icons/icon128.png',
+  // },
+});
 
 export default defineConfig({
-  // @see https://github.com/crxjs/chrome-extension-tools/issues/696
-  server: {
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      port: 5173,
-    },
-  },
-  // prevent src/ prefix on extension urls
-  root: resolve(__dirname, 'src'),
-  publicDir: resolve(__dirname, 'public'),
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    rollupOptions: {
-      input: {
-        // see web_accessible_resources in the manifest config
-        welcome: join(__dirname, 'src/welcome/welcome.html'),
-      },
-      output: {
-        chunkFileNames: 'assets/chunk-[hash].js',
-      },
-    },
-  },
   plugins: [react(), crx({ manifest })],
 });
